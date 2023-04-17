b45109a8e02611e602ed45713c2de06a14af40c3
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-04-17 08:33:30 -0600

    Init commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+    './src/**/*.js',
+    './*.html',
+    './src/*.jsx',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
+    },
+    extend: {
+      colors: {
+        brightRed: '#FF0000'
+      }
+    },
+    plugins: []
+  }
+}
